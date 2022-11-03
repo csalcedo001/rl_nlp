9a21a0d725596a57a034b2326303d373e5d142bb
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-11-03 19:02:39 +0000

    Added env wrapper for render obs and tested on random agent

---
+++ env.py
@@ -0,0 +1,33 @@
+import gym
+import numpy as np
+
+
+class RenderObsEnv(gym.Wrapper):
+    def __init__(self, env, *args, **kwargs):
+        super().__init__(env, *args, **kwargs)
+
+        self.env = env
+
+    @property
+    def observation_space(self):
+        return self.env.observation_space
+
+    @property
+    def action_space(self):
+        return self.env.action_space
+
+    def step(self, action):
+        _, reward, done, _, info = self.env.step(action)
+        obs = self._get_obs()
+
+        return obs, reward, done, False, info
+
+    def reset(self):
+        self.env.reset()
+
+        obs = self._get_obs()
+
+        return obs
+
+    def _get_obs(self):
+        return self.env.render().transpose((2, 0, 1))
---
+++ random_action.py
@@ -0,0 +1,30 @@
+import gym
+import torch
+import numpy as np
+
+from dreamer import Dreamer
+from env import RenderObsEnv
+
+env = gym.make('Acrobot-v1', render_mode='rgb_array')
+env = RenderObsEnv(env)
+
+episodes = 10
+max_iteration = 1000
+
+for episode in range(episodes):
+    state = env.reset()
+
+    for iteration in range(max_iteration):
+        state = torch.from_numpy(state).float()
+        state = state.reshape(1, *state.shape)
+
+        action = env.action_space.sample()
+
+        next_state, reward, done, _, info = env.step(action)
+
+        print('Episode {}: reward: {}'.format(episode, reward))
+
+        if done:
+            break
+
+        state = next_state
