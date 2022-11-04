9378517e53f30db73b4e88bd6ca0c6f5726e0416
Cesar Salcedo <cesar.salcedo@utec.edu.pe>
2022-11-04 02:06:36 +0000

    Updated random_action.py

--- experiments/2022-11-03/random_action.py
+++ experiments/2022-11-03/random_action.py
@@ -1,33 +1,34 @@
 import gym
 import torch
 import numpy as np
+from tqdm import tqdm
 
-from dreamer import Dreamer
 from dreamer.env import RenderObsWrapper
 
 env = gym.make('Acrobot-v1', render_mode='rgb_array')
 env = RenderObsWrapper(env)
 
 episodes = 10
-max_iteration = 1000
+max_iteration = 100
 
-print("Started training")
+print("Starting rollouts...")
 for episode in range(episodes):
     state = env.reset()
 
-    for iteration in range(max_iteration):
+    total_reward = 0.
+
+    for iteration in tqdm(range(max_iteration)):
         state = torch.from_numpy(state).float()
         state = state.reshape(1, *state.shape)
 
         action = env.action_space.sample()
 
         next_state, reward, done, _, info = env.step(action)
-
-        # print('Episode {}: reward: {}'.format(episode, reward))
+        total_reward += reward
 
         if done:
             break
 
         state = next_state
-    
-    print("Finished episode {}".format(episode))
+
+    print('Episode: {}, reward: {}'.format(episode, reward))
